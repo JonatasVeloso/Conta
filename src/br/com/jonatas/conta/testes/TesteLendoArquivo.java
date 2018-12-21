accf9c388dadb685829c4a32be033a4ec5c365ac
Jonatas Veloso <jntsvls@gmail.com>
2018-12-21 02:54:31 -0200

    Teste Lendo Arquivo

---
+++ src/br/com/jonatas/conta/testes/TesteLendoArquivo.java
@@ -0,0 +1,33 @@
+package br.com.jonatas.conta.testes;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TesteLendoArquivo {
+	public static void main(String[] args) throws IOException{
+		FileInputStream fis = new FileInputStream("TesteLeitura.txt");
+		InputStreamReader isr = new InputStreamReader(fis);
+		BufferedReader br 	= new BufferedReader(isr);
+		
+		String linha = br.readLine();
+		
+		while(linha != null) {
+			if(linha.length() > 3) {
+				if(linha.substring(0, 3).equals("505")) {
+					System.out.println("Nota: " + linha.substring(4, 14));
+				}
+				if(linha.substring(0, 3).equals("506")) {
+					System.out.println("CNPJ: " + linha.substring(4, 18));
+				}
+			
+			}
+			
+			linha = br.readLine();
+		}
+		
+		
+	}
+}
